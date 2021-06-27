c9973dd86aa6864d4952da75a76c38400263c8ce
LOVINE <cynthia.lovine@student.moringaschool.com>
2021-06-27 16:32:48 +0300

    modified the behaviour of the make delivery button

--- static/javascript/script.js
+++ static/javascript/script.js
@@ -2,7 +2,7 @@ $(function () {
     $('.summary').hide();
     $('.cdata-overlay').hide();
 //Get inputs
-    $("#placeorder").click(function () {
+    $("#checkout").click(function () {
         let flavour = $(".flavour option:selected").val();
         let size = $("#size option:selected").val();
         let crust = $("#crust option:selected").val();
@@ -14,7 +14,7 @@ $(function () {
         let order = (f, s, c, t, n, total) => {
             return {f, s, c, t, n, total};
         };
-    
+        
         //check price
         let price, totalPrice;
         switch (flavour) {
@@ -375,3 +375,51 @@ $(function () {
                 break;
 
         }
+         //Execute order function
+         let newOrder = order(flavour, size, crust, topping, number, totalPrice);
+         console.log(newOrder); // test func
+ 
+         //create a new object
+         // let myOrder = JSON.stringify(JSON.parse(newOrder));
+ 
+         //Write to the order
+         $('.summary').slideDown(2000);
+         $('.cdata-overlay').slideUp();
+         $('#list').slideDown();
+         $('.deliver').show(1000);
+         $('.delivernot').show(1000);
+ 
+         $('#list').text(" ");
+         $("#list").append("<br>" + "Flavour :   " + newOrder.f + "<br>" + "Size :   "
+             + newOrder.s + "<br>" + "Crust :     "
+             + newOrder.c + "<br>" + "Toppings :     "
+             + newOrder.t + "<br>" + " Number of pizzas :    "
+             + newOrder.n + "<br>" + "Total Price :  "
+             + newOrder.total + "<br><br>").css('font-family', 'system-ui').css('font-size', '24px');
+     });
+ 
+     //Deliver
+     $(".deliver").click(function () {
+         $('.summary').slideUp();
+         $('#list').slideUp();
+         $('.summary').text("Provide location details").slideDown();
+         $('.deliver').hide(1000);
+         $('.delivernot').hide(1000);
+         $('.cdata-overlay').slideDown();
+     });
+ 
+        $("#dday").click(function(){
+            alert("Your package is on the way")
+        });
+        
+     //Pick Up
+     $(".delivernot").click(function () {
+       alert("See You Soon!")
+     });
+ 
+     //Scrollify
+     $(function () {
+         $.scrollify.move('#sum-order');
+     });
+ });
+ 
